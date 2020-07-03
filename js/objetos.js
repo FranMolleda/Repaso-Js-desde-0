747a37f5fbf9bdbfcd7dcd433f4cf45cf1506524
Fran <fmjerez@icloud.com>
2020-07-03 11:09:11 +0200

    Object Literal

---
+++ js/objetos.js
@@ -0,0 +1,9 @@
+//Objetos
+
+//Objet literal
+
+let persona = { nombre: "Fran", profesion: "Desarrollador Web", edad: 43 };
+
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona["edad"]);
