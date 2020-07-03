d8589cbaa411582ad0832a4fc68d4ed8790708f4
Fran <fmjerez@icloud.com>
2020-07-03 11:19:17 +0200

    Object Constructor

--- js/objetos.js
+++ js/objetos.js
@@ -7,3 +7,18 @@ let persona = { nombre: "Fran", profesion: "Desarrollador Web", edad: 43 };
 console.log(persona);
 console.log(persona.nombre);
 console.log(persona["edad"]);
+
+// Object Constructor
+
+function Tarea(nombre, urgencia) {
+  this.nombre = nombre;
+  this.urgencia = urgencia;
+}
+
+//Crear una nueva tarea
+
+const tarea1 = new Tarea("Aprender Js y React", "Urgente");
+const tarea2 = new Tarea("Montar en bici", "No es urgente");
+
+console.log(tarea1);
+console.log(tarea2);
