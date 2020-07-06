8a20e2ab66fdbd20c691a7331c7832d02b5b16fe
Fran <fmjerez@icloud.com>
2020-07-06 12:55:27 +0200

    Promesas con Ajax

--- js/promesas.js
+++ js/promesas.js
@@ -19,3 +19,37 @@ aplicarDescuento
   .catch((error) => {
     console.log(error);
   });
+
+//Promesas con Ajax
+
+const descargarUsuarios = (cantidad) =>
+  new Promise((resolve, reject) => {
+    //Pasar la cantidad a la api
+    const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
+
+    //llamado ajax
+    const xhr = new XMLHttpRequest();
+
+    //abrir la conexion
+    xhr.open("GET", api, true);
+
+    //on load
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText).results);
+      } else {
+        reject(Error(xhr.statusText));
+      }
+    };
+
+    //opcional (on error)
+    xhr.onerror = (error) => reject(Error(xhr.statusText));
+
+    //send
+    xhr.send();
+  });
+
+descargarUsuarios(10).then(
+  (miembros) => console.log(miembros),
+  (error) => console.error(new Error("Hubo un error" + error))
+);
