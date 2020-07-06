991158b7e667053d104a253c29ccff4bdd6d87f7
Fran <fmjerez@icloud.com>
2020-07-06 12:23:11 +0200

    Promesas

---
+++ js/promesas.js
@@ -0,0 +1,21 @@
+//Promesas
+
+const aplicarDescuento = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let descuento = true;
+
+    if (descuento) {
+      resolve("Descuento aplicado");
+    } else {
+      reject("No se pudo aplicar el descuento");
+    }
+  }, 2000);
+});
+
+aplicarDescuento
+  .then((resultado) => {
+    console.log(resultado);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
