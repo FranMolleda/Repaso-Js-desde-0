ebac40332c0ba494602725933cd2ceed457e3191
Fran <fmjerez@icloud.com>
2020-07-09 09:25:39 +0200

    Exportando Clases

--- js/modulos/app.js
+++ js/modulos/app.js
@@ -1,5 +1,8 @@
 import casaysilla from "./modulos.js";
+//Importamos una función
 import { crearTarea, tareaCompletada } from "./exportarFuncion.js";
+//Importamos una clase
+import Tarea from "./exportarClases.js";
 
 console.log(casaysilla);
 
@@ -10,3 +13,8 @@ console.log(tarea1);
 setTimeout(() => {
   tareaCompletada();
 }, 2000);
+
+//Importamos Clases
+
+const tarea2 = new Tarea("Aprender JS", "Alta");
+tarea2.mostrar();
